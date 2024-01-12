65a123001b44c61a502f99f1802b9e1ef57d2a73
Cicak Kayang <eriscapricron@gmail.com>
2024-01-12 09:39:05 +0700

    update facebook pixel

---
+++ src/app/FacebookPixel.jsx
@@ -0,0 +1,28 @@
+// FacebookPixel.js
+
+import React from 'react';
+
+const FacebookPixel = () => (
+  <>
+    {/* Facebook Pixel Code */}
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+         !function(f,b,e,v,n,t,s)
+         {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+         n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+         if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+         n.queue=[];t=b.createElement(e);t.async=!0;
+         t.src=v;s=b.getElementsByTagName(e)[0];
+         s.parentNode.insertBefore(t,s)}(window,document,'script',
+         'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '364087169562944'); 
+         fbq('track', 'PageView');
+        `,
+      }}
+    />
+    {/* End Facebook Pixel Code */}
+  </>
+);
+
+export default FacebookPixel;
